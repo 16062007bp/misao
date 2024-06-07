03cd238963b878c7e8fa683b4d349d315bcb698a
16062007bp <106752255+16062007bp@users.noreply.github.com>
2024-06-07 08:47:38 -0300

    sapa

---
+++ JS script.js
@@ -0,0 +1,15 @@
+const caixaprincpal = document.querySelector("caixa-principal")
+const caixaprincpal = document.querySelector("caixa-pergunta")
+const caixaprincpal = document.querySelector("caixa-alternativa")
+const caixaprincpal = document.querySelector("caixa-resultado")
+const caixaprincpal = document.querySelector("caixa-resultado")
+ const pergunta =[
+    {//primeiro objeto da lista de pergunta
+    enunciado: "pergunta1",
+    alternativas: ["alternatival", "alternativa2"]
+    },
+    {//segundo objeto da lista de perguntas
+    enunciado: "pergunta2",
+    alternativas: ["alternatival", "alternativa2"]
+    }
+ ];
